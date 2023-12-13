ea169b9a6a61a10f23b61ebf929fdc76b665c3a4
Rodrigo <rodrigocavalcante.pro@gmail.com>
2023-12-13 01:16:49 -0300

    Initial Commit

---
+++ Afazeres/AfazeresApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfazeresApp.swift
+//  Afazeres
+//
+//  Created by Rodrigo Cavalcante on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AfazeresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

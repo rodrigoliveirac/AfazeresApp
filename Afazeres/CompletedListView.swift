0fa4cd82195b946f12ce7fa7e53210866b733176
Rodrigo <rodrigocavalcante.pro@gmail.com>
2023-12-15 00:58:52 -0300

    Creating Incomplete and completed task list

---
+++ Afazeres/CompletedListView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedListView.swift
+//  Afazeres
+//
+//  Created by Rodrigo Cavalcante on 15/12/23.
+//
+
+import SwiftUI
+
+struct CompletedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedListView()
+}
---
+++ Afazeres/IncompleteListView.swift
@@ -0,0 +1,18 @@
+//
+//  IncompleteListView.swift
+//  Afazeres
+//
+//  Created by Rodrigo Cavalcante on 15/12/23.
+//
+
+import SwiftUI
+
+struct IncompleteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IncompleteListView()
+}
